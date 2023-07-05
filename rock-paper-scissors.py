4ac19996ed18f4a43f244fd8eba707276d0edb0c
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-05 18:37:45 +0200

    Update rock-paper-scissors.py

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -6,3 +6,4 @@ def determine_winner(player_choice, computer_choice):
         return "It's a tie!"
     elif (
         (player_choice == 'rock' and computer_choice == 'scissors') or
+        (player_choice == 'paper' and computer_choice == 'rock') or
