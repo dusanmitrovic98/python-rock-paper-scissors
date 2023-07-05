e4be976f4ee1a1923463c532bd71870647ee4d0a
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-05 18:57:22 +0200

    Update rock-paper-scissors.py

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -7,3 +7,4 @@ def determine_winner(player_choice, computer_choice):
     elif (
         (player_choice == 'rock' and computer_choice == 'scissors') or
         (player_choice == 'paper' and computer_choice == 'rock') or
+        (player_choice == 'scissors' and computer_choice == 'paper')
